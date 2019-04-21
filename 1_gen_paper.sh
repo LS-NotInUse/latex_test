c9ce85a2f5cd2a03d83cbf968623ce0b612db6f3
Louis Sung <sjf.csie@gmail.com>
2019-04-21 23:39:47 +0800

    Update scripts

--- 1_gen_paper.sh
+++ 1_gen_paper.sh
@@ -5,15 +5,16 @@ if [ ! -f "build/Paper.aux" ]; then    # make sure to ref citation for the first
 
 pdflatex -output-directory=build/ Paper.tex > /dev/null
 # print important msg only
-printf "** Conference Paper **
-Before submitting the final camera ready copy, remember to:
-
- 1. Manually equalize the lengths of two columns on the last page
- of your paper;
-
- 2. Ensure that any PostScript and/or PDF output post-processing
- uses only Type 1 fonts and that every step in the generation
- process uses the appropriate paper size.
-"
+echo "\
+log
+   | ** Conference Paper **
+   | Before submitting the final camera ready copy, remember to:
+   | 
+   |  1. Manually equalize the lengths of two columns on the last page
+   |  of your paper;
+   | 
+   |  2. Ensure that any PostScript and/or PDF output post-processing
+   |  uses only Type 1 fonts and that every step in the generation
+   |  process uses the appropriate paper size."
 
 echo -e "\e[1;36mDone\e[0m"
